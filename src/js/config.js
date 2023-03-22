9cc4803ebc3605b1e0b8ba9a0bc38f3cfc26b8d8
Matt Seidel <mseidel819@gmail.com>
2023-03-22 08:24:03 -0500

    add gitignoe and new keys

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
-export const API_KEY = "BPUFvkh8oJ2OQUbe0KDpOmlvbx2assWOaLKU3DD1ExENu8JUDp";
-export const API_SECRET = "0luwRLgUB1WXiMzJkcoVw26TbEKCUsYvuNnexYES";
+export const API_KEY = "bTyjgQZKXYRFd47brxfElTRNHMsf6YO1xiQURfxf8EYUwLObYy";
+export const API_SECRET = "BslahT4sktNeuud42YRRcp5GDCLy09Bhla6q2wdY";
 export const API_TOKEN_URL = "https://api.petfinder.com/v2/oauth2/token";
 export const API_URL = "https://api.petfinder.com/v2/animals";
 export const API_ORG = "https://api.petfinder.com/v2/organizations";
