1b216501c45434cd4ae96dfe3730c5b55a851728
Matt Seidel <mseidel819@gmail.com>
2023-03-22 11:47:11 -0500

    retry

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_KEY = "bTyjgQZKXYRFd47brxfElTRNHMsf6YO1xiQURfxf8EYUwLObYy";
+export const API_SECRET = "BslahT4sktNeuud42YRRcp5GDCLy09Bhla6q2wdY";
+export const API_TOKEN_URL = "https://api.petfinder.com/v2/oauth2/token";
+export const API_URL = "https://api.petfinder.com/v2/animals";
+export const API_ORG = "https://api.petfinder.com/v2/organizations";
+export const TIMEOUT_SEC = 10;
